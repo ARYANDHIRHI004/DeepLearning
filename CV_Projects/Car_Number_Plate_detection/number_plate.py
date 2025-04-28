231d428c36ebda32e997461ef31f7badedf4499c
aryandhirhi004 <aryandhirhi004@gmail.com>
2025-04-28 22:25:35 +0530

    car number plate detector project completed

--- CV_Projects/Car_Number_Plate_detection/number_plate.py
+++ CV_Projects/Car_Number_Plate_detection/number_plate.py
@@ -28,19 +28,18 @@ while True:
 
         if area > min_area:
             cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 2)
-            cv2.putText(img, 'Licence Plate', (x,y-5), cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 255), 2)
+            cv2.putText(img, 'Licence Plate', (x,y-5), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255, 0, 255), 2)
             img_roi = img[y:y + h, x:x + w]
-            cv2.imshow(img_roi)
+            cv2.imshow("ROI", img_roi)
 
     cv2.imshow("Result", img)
 
     # saving the plate when 's' key is pressed
-
     if cv2.waitKey(1) & 0xFF == ord('s'):
         cv2.imwrite("plate_" + str(count) + ".jpg" ,img_roi)
         cv2.rectangle(img, (0,200), (640,300), (0,255,0), cv2.FILLED)
-        cv2.putText(img, "plate Saved", (150, 265), cv2.FONT_HERSHEY_COMPLEX, 2,(0,0,255), 2)
-        cv2.imshow("Result", img)
+        cv2.putText(img, "plate Saved", (150, 265), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2,(0,0,255), 2)
+        cv2.imshow("Results", img)
         cv2.waitKey(500)
         count += 1
 
