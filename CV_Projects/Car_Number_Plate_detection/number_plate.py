033fad857956df94f924751addc43470446e86d3
aryandhirhi004 <aryandhirhi004@gmail.com>
2025-04-28 22:05:07 +0530

    Car number plate detection

---
+++ CV_Projects/Car_Number_Plate_detection/number_plate.py
@@ -0,0 +1,48 @@
+import cv2
+
+harcascade = "E:\\aryan\Aryancoding\DataScience\DeepLearning\CV_Projects\Car_Number_Plate_detection\models\haarcascade_russian_plate_number.xml"
+
+cap = cv2.VideoCapture(0) # capture webcam
+
+cap.set(3, 640) # set width
+cap.set(4, 480) # set hight
+
+min_area = 500
+
+count = 0
+
+while True:
+    # reading frame from webcame
+    success, img = cap.read()
+
+    # creating a licence plate classifier
+    plate_cascade = cv2.CascadeClassifier(harcascade)
+
+    # convert the frame to grayscale
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    plates = plate_cascade.detectMultiScale(img_gray, 1.1, 4)
+
+    for (x, y, w, h) in plates:
+        area = w * h
+
+        if area > min_area:
+            cv2.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 2)
+            cv2.putText(img, 'Licence Plate', (x,y-5), cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 255), 2)
+            img_roi = img[y:y + h, x:x + w]
+            cv2.imshow(img_roi)
+
+    cv2.imshow("Result", img)
+
+    # saving the plate when 's' key is pressed
+
+    if cv2.waitKey(1) & 0xFF == ord('s'):
+        cv2.imwrite("plate_" + str(count) + ".jpg" ,img_roi)
+        cv2.rectangle(img, (0,200), (640,300), (0,255,0), cv2.FILLED)
+        cv2.putText(img, "plate Saved", (150, 265), cv2.FONT_HERSHEY_COMPLEX, 2,(0,0,255), 2)
+        cv2.imshow("Result", img)
+        cv2.waitKey(500)
+        count += 1
+
+
+
